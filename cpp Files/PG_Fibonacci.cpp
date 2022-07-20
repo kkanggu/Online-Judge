34acd4e52c44ef07318945f2445fdb076b450ef3
kkanggu <rica742244@gmail.com>
2022-07-20 22:40:00 +0900

    DP, matrix is better

---
+++ cpp Files/PG_Fibonacci.cpp
@@ -0,0 +1,16 @@
+int solution ( int n )
+{
+	int irgDP [ 3 ] = { 0 , 0 , 1 } ;
+	
+	
+	
+	for ( int i = 1 ; i < n ; ++i )
+	{
+		irgDP [ 0 ] = irgDP [ 1 ] ;
+		irgDP [ 1 ] = irgDP [ 2 ] ;
+		irgDP [ 2 ] = ( irgDP [ 0 ] + irgDP [ 1 ] ) % 1234567 ;
+	}
+	
+	
+	return irgDP [ 2 ] ;
+}
